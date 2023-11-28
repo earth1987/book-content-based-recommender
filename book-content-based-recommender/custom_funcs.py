cfa047e93803bb0c2770fc441a75c718fe87035c
earth1987 <billypitchford@gmail.com>
2023-11-28 11:54:04 +0000

    amended cross-validation functions, distinguished baseline & content-based models

--- book-content-based-recommender/custom_funcs.py
+++ book-content-based-recommender/custom_funcs.py
@@ -2,32 +2,54 @@ import pandas as pd
 import numpy as np
 import random
 from sklearn.model_selection import KFold
+import nltk
+import re
 
 ###############################################################################################
 
-def calculate_cv_scores(all_user_ratings, n_splits, top_k, all_books, all_user_toread, recommendation_sys):
-    """
-    Function to calculate kfold cross-validated precision@k, recall@k, mean average precision@k and ndcg.
+def preprocess_text(text, stopwords, lemmatizer):
+    """ 
+    Function to process text
     
     Parameters:
-    - all_user_ratings: Table of user ratings for books
+    - text: Input string
+    - stopwords: List of stopwords to remove
+    - lemmatizer: Instantiated lemmatizer object
+
+    Returns: Processed string.
+    """
+    text = str(text)
+    text = text.lower() # Convert to lowercase
+    text = re.sub('[^a-z\s]', '', text)  # Remove non-alphabetic characters
+    tokens = nltk.word_tokenize(text) # Tokenize
+    tokens = [lemmatizer.lemmatize(t) for t in tokens if t not in stopwords] # Lemmatize and remove stopwords
+    return " ".join(tokens)    
+
+###############################################################################################
+
+def calculate_baseline_cv_score(recommendation_sys, all_user_ratings, all_user_toread, all_books, n_sample, n_splits, top_k):
+    """
+    Function to calculate kfold cross-validated precision@k, recall@k and mean average precision@k
+    
+    Parameters:
+    - recommendation_sys: Chosen recommendation system (e.g. popularity_rec) 
+    - all_user_ratings: DataFrame containing ratings history for all users.
+    - all_user_toread: DataFrame containing to read list for all users.
+    - all_books: DataFrame containing metadata for all books.
+    - n_sample: Number of recommendations to generate.
     - n_splits: Number of kfold splits
     - top_k: Number of top recommendations to consider
-    - all_books: Table of book metadata
-    - all_user_toread: To read list for all users
-    - recommendation_sys: Chosen recommendation system (e.g. popularity_rec) 
-    
+
     Returns:
-    - precision_at_k: A dictionary containing Precision@K scores for each fold
-    - recall_at_k: A dictionary containing Recall@k scores for each fold
-    - map_at_k: A dictionary containing Mean average precision @ k scores for each fold
+    - avg_precision_at_k: Average Precision@K across kfolds
+    - avg_recall_at_k: Average Recall@k across kfolds
+    - avg_map_at_k: Average Mean average precision @ k across kfolds
     """
-    
-    # Output dictionaries
     precision_at_k = {}
     recall_at_k = {}
     map_at_k = {}
-    
+
+    # Iterate over kfolds
     kf = KFold(n_splits, shuffle=True, random_state=0)
     for n, (train_index, valid_index) in enumerate(kf.split(all_user_ratings)):
 
@@ -35,11 +57,17 @@ def calculate_cv_scores(all_user_ratings, n_splits, top_k, all_books, all_user_t
         train_data = all_user_ratings.iloc[train_index]
         valid_data = all_user_ratings.iloc[valid_index]
 
-        # Calculate average evaluation metric scores across users for kth fold
-        precision_at_k[n], recall_at_k[n], map_at_k[n] = calculate_avg_scores(train_data, valid_data, top_k, all_books, all_user_toread, recommendation_sys)
+        # Filter new users out of validation set
+        unique_train_users = train_data["user_id"].unique()
+        unique_train_users = set(unique_train_users)
+        valid_data = valid_data.loc[valid_data["user_id"].isin(
+            unique_train_users)]
+
+        # Compute evaluation metrics and update relevant dictionaries with scores
+        precision_at_k[n], recall_at_k[n], map_at_k[n] = calculate_baseline_kfold_score(recommendation_sys, train_data, valid_data, all_user_toread, all_books, n_sample, top_k)
 
 
-    # Calculate average evaluation metric scores across k folds
+    # Compute average evaluation metric scores
     avg_precision_at_k = sum(precision_at_k.values()) / len(precision_at_k)
     avg_recall_at_k = sum(recall_at_k.values()) / len(recall_at_k)
     avg_map_at_k = sum(map_at_k.values()) / len(map_at_k)
@@ -48,17 +76,136 @@ def calculate_cv_scores(all_user_ratings, n_splits, top_k, all_books, all_user_t
 
 ###############################################################################################
 
-def calculate_avg_scores(train_data, valid_data, top_k, all_books, all_user_toread, recommendation_sys):
+def calculate_baseline_kfold_score(recommendation_sys, train_data, valid_data, all_user_toread, all_books, n_sample, top_k):
     """
-    Function to calculate precision, recall, mean average precision and ndcg given training/validation data.
+    Function to calculate precision, recall and mean average precision given training/validation data.
 
     Parameters:
+    - recommendation_sys: Chosen recommendation system (e.g. popularity_rec)
     - train_data: List of user ratings for each book
     - valid_data: List of user ratings for each book
+    - all_user_toread: DataFrame containing to read list for all users.
+    - all_books: DataFrame containing metadata for all books.
+    - n_sample: Number of recommendations to generate.
     - top_k: Number of top recommendations to consider
-    - all_books: Table of book metadata
-    - all_user_toread: To read list for all users
+    
+    Returns:
+    - overall_precision: Average precision@K across users in the validation data
+    - overall_recall: Average Recall@k across users in the validation data
+    - overall_map: Average Mean average precision @ k across users in the validation data
+    """
+    user_recommendations = {}
+    user_history = {}
+    user_precision = {}
+    user_recall = {}
+    user_map = {}
+
+    # Iterate over random sample of users from the validation set
+    valid_user_ids = valid_data["user_id"].unique()
+    valid_user_ids = np.random.choice(valid_user_ids, size=int(
+        len(valid_user_ids)*0.01), replace=False)
+    for user_id in valid_user_ids:
+        
+        # Update dictionary with n_sample predicted items for the user
+        user_recommendations[user_id] = recommendation_sys(user_id, train_data, all_user_toread, all_books, n_sample)
+    
+        # Update dictionary with actual rated items for the user
+        user_history[user_id] = valid_data.loc[valid_data["user_id"] == user_id, "book_id"].tolist()
+
+        # Compute evaluation metrics and update relevant dictionaries with scores
+        predicted = user_recommendations.get(user_id, [])
+        actual = user_history.get(user_id, [])
+        user_precision[user_id] = calculate_precision_at_k(actual, predicted, top_k)
+        user_recall[user_id] = calculate_recall_at_k(actual, predicted, top_k)
+        user_map[user_id] = calculate_mean_average_precision_at_k(actual, predicted, top_k)
+        
+    # Compute average evaluation metric scores
+    overall_precision = sum(user_precision.values()) / len(user_precision)
+    overall_recall = sum(user_recall.values()) / len(user_recall)
+    overall_map = sum(user_map.values()) / len(user_map)
+
+    return overall_precision, overall_recall, overall_map
+
+###############################################################################################
+
+def calculate_cv_score(recommendation_sys, book_matrix, all_user_ratings, all_user_toread, all_books, stopwords, lemmatizer, vectorizer, n_sample, n_splits, top_k):
+    """
+    Function to calculate kfold cross-validated precision@k, recall@k and mean average precision@k.
+
+    Parameters:
     - recommendation_sys: Chosen recommendation system (e.g. popularity_rec) 
+    - book_matrix: Matrix of book vectors
+    - all_user_ratings: DataFrame containing ratings history for all users.
+    - all_user_toread: DataFrame containing to read list for all users.
+    - all_books: DataFrame containing metadata for all books.
+    - stopwords: Set of stop words
+    - lemmatizer: Instantiated lemmatizer
+    - n_sample: Number of recommendations to generate.
+    - n_splits: Number of kfold splits
+    - top_k: Number of top recommendations to consider
+
+    Returns:
+    - avg_precision_at_k: Average Precision@K across kfolds
+    - avg_recall_at_k: Average Recall@k across kfolds
+    - avg_map_at_k: Average Mean average precision @ k across kfolds
+    """
+
+    precision_at_k = {}
+    recall_at_k = {}
+    map_at_k = {}
+
+    # Iterate over kfolds
+    kf = KFold(n_splits, shuffle=True, random_state=0)
+    for n, (train_index, valid_index) in enumerate(kf.split(all_user_ratings)):
+
+        # Create training/validation sets
+        train_data = all_user_ratings.iloc[train_index]
+        valid_data = all_user_ratings.iloc[valid_index]
+
+        # Filter new users out of validation set
+        unique_train_users = train_data["user_id"].unique()
+        unique_train_users = set(unique_train_users)
+        valid_data = valid_data.loc[valid_data["user_id"].isin(
+            unique_train_users)]
+
+        # Vectorise user profiles
+        all_user_profiles = train_data.groupby("user_id")["book_profile"].agg(
+            lambda x: " ".join(x)).rename("user_profile")
+        all_user_profiles = all_user_profiles.apply(
+            lambda x: preprocess_text(x, stopwords, lemmatizer))
+        user_matrix = vectorizer.transform(all_user_profiles.to_numpy())
+        user_matrix_ids = all_user_profiles.index.to_numpy()
+
+        # Compute similarity matrix
+        cosine_similarity_matrix = cosine_similarity(user_matrix, book_matrix)
+
+        # Compute evaluation metrics and update relevant dictionaries with scores
+        precision_at_k[n], recall_at_k[n], map_at_k[n] = calculate_kfold_score(
+            recommendation_sys, cosine_similarity_matrix, user_matrix_ids, train_data, valid_data, all_user_toread, all_books, n_sample, top_k)
+
+    # Compute average evaluation metric scores
+    avg_precision_at_k = sum(precision_at_k.values()) / len(precision_at_k)
+    avg_recall_at_k = sum(recall_at_k.values()) / len(recall_at_k)
+    avg_map_at_k = sum(map_at_k.values()) / len(map_at_k)
+
+    return avg_precision_at_k, avg_recall_at_k, avg_map_at_k
+
+###############################################################################################
+    
+def calculate_kfold_score(recommendation_sys, cosine_similarity_matrix, user_matrix_ids, train_data, valid_data, all_user_toread, all_books, n_sample, top_k):
+    """
+    Function to calculate precision, recall and mean average precision given training/validation data.
+
+    Parameters:
+    - recommendation_sys: Chosen recommendation system (e.g. popularity_rec)
+    - cosine_similarity_matrix: Each row corresponds to a user and each column corresponds to a book
+    - user_matrix_ids: Numpy array containing user_id corresponding to each row in the similarity matrix
+    - train_data: List of user ratings for each book
+    - valid_data: List of user ratings for each book
+    - all_user_toread: DataFrame containing to read list for all users.
+    - all_books: DataFrame containing metadata for all books.
+    - n_sample: Number of recommendations to generate.
+    - top_k: Number of top recommendations to consider
 
     Returns:
     - overall_precision: Average precision@K across users in the validation data
@@ -71,33 +218,143 @@ def calculate_avg_scores(train_data, valid_data, top_k, all_books, all_user_tore
     user_recall = {}
     user_map = {}
 
-    # Randomly sample user ids from the validation set
-    user_ids = list(valid_data["user_id"].unique())
-    sampled_user_ids = random.sample(user_ids, int(0.1*len(user_ids)))
+    # Iterate over random sample of users from the validation set
+    valid_user_ids = valid_data["user_id"].unique()
+    valid_user_ids = np.random.choice(valid_user_ids, size=int(
+        len(valid_user_ids)*0.01), replace=False)
+    for user_id in valid_user_ids:
 
-    for id in sampled_user_ids:
-        
-        # Output a dictionary containing 50 predicted items for each user.
-        user_recommendations[id] = recommendation_sys(id, train_data, 50, all_books, all_user_toread)
-    
-        # Output a dictionary containing actual rated items for each user.
-        user_history[id] = valid_data.loc[valid_data["user_id"] == id, "book_id"].tolist()
+        # Update dictionary with n_sample predicted items for the user
+        user_recommendations[user_id] = recommendation_sys(
+            user_id, cosine_similarity_matrix, user_matrix_ids, train_data, all_user_toread, all_books, n_sample)
 
-        # Get lists of predicted & actual items for the user, default to an empty list
-        predicted = user_recommendations.get(id, [])
-        actual = user_history.get(id, [])
-    
-        # Output a dictionary containing metric scores for each user 
+        # Update dictionary with actual rated items for the user
+        user_history[user_id] = valid_data.loc[valid_data["user_id"]
+                                               == id, "book_id"].tolist()
+
+        # Compute evaluation metrics and update relevant dictionaries with scores
+        predicted = user_recommendations.get(user_id, [])
+        actual = user_history.get(user_id, [])
         user_precision[id] = calculate_precision_at_k(actual, predicted, top_k)
         user_recall[id] = calculate_recall_at_k(actual, predicted, top_k)
-        user_map[id] = calculate_mean_average_precision_at_k(actual, predicted, top_k)
-        
-    # Average metric over all users
+        user_map[id] = calculate_mean_average_precision_at_k(
+            actual, predicted, top_k)
+
+    # Compute average evaluation metric scores
     overall_precision = sum(user_precision.values()) / len(user_precision)
     overall_recall = sum(user_recall.values()) / len(user_recall)
     overall_map = sum(user_map.values()) / len(user_map)
 
     return overall_precision, overall_recall, overall_map
+
+###############################################################################################
+
+# def calculate_cv_scores(recommendation_sys, book_matrix, all_user_ratings, all_user_toread, all_books, n_sample, n_splits, top_k):
+#     """
+#     Function to calculate kfold cross-validated precision@k, recall@k and mean average precision@k.
+    
+#     Parameters:
+#     - recommendation_sys: Chosen recommendation system (e.g. popularity_rec) 
+#     - book_matrix: Matrix of book vectors
+#     - all_user_ratings: DataFrame containing ratings history for all users.
+#     - all_user_toread: DataFrame containing to read list for all users.
+#     - all_books: DataFrame containing metadata for all books.
+#     - n_sample: Number of recommendations to generate.
+#     - n_splits: Number of kfold splits
+#     - top_k: Number of top recommendations to consider
+
+#     Returns:
+#     - avg_precision_at_k: Average Precision@K across kfolds
+#     - avg_recall_at_k: Average Recall@k across kfolds
+#     - avg_map_at_k: Average Mean average precision @ k across kfolds
+#     """
+    
+#     # Output dictionaries
+#     precision_at_k = {}
+#     recall_at_k = {}
+#     map_at_k = {}
+    
+#     kf = KFold(n_splits, shuffle=True, random_state=0)
+#     for n, (train_index, valid_index) in enumerate(kf.split(all_user_ratings)):
+
+#         # Create training/validation sets
+#         train_data = all_user_ratings.iloc[train_index]
+#         valid_data = all_user_ratings.iloc[valid_index]
+
+#         # Create user vectors
+#         all_user_profiles = train_data.groupby("user_id")["book_profile"].agg(lambda x: " ".join(x)).rename("user_profile")
+#         all_user_profiles = all_user_profiles.apply(lambda x: preprocess_text(x, stopwords, lemmatizer))
+#         all_user_vectors = tfidf_vectorizer.transform(all_user_profiles.tolist())
+
+#         # Compute similarity matrix
+#         cosine_similarity_matrix = cosine_similarity(all_user_vectors, book_matrix)
+                
+#         # Calculate average evaluation metric scores across users for kth fold
+#         precision_at_k[n], recall_at_k[n], map_at_k[n] = calculate_kfold_score(recommendation_sys, cosine_similarity_matrix, train_data, valid_data, all_user_toread, all_books, n_sample, top_k)
+
+
+#     # Calculate average evaluation metric scores across k folds
+#     avg_precision_at_k = sum(precision_at_k.values()) / len(precision_at_k)
+#     avg_recall_at_k = sum(recall_at_k.values()) / len(recall_at_k)
+#     avg_map_at_k = sum(map_at_k.values()) / len(map_at_k)
+        
+#     return avg_precision_at_k, avg_recall_at_k, avg_map_at_k
+
+###############################################################################################
+
+# def calculate_kfold_score(recommendation_sys, cosine_similarity_matrix, train_data, valid_data, all_user_toread, all_books, n_sample, top_k):
+
+#     """
+#     Function to calculate precision, recall and mean average precision given training/validation data.
+
+#     Parameters:
+#     - recommendation_sys: Chosen recommendation system (e.g. popularity_rec)
+#     - cosine_similarity_matrix: Each row corresponds to a user and each column corresponds to a book
+#     - train_data: List of user ratings for each book
+#     - valid_data: List of user ratings for each book
+#     - all_user_toread: DataFrame containing to read list for all users.
+#     - all_books: DataFrame containing metadata for all books.
+#     - n_sample: Number of recommendations to generate.
+#     - top_k: Number of top recommendations to consider
+    
+#     Returns:
+#     - overall_precision: Average precision@K across users in the validation data
+#     - overall_recall: Average Recall@k across users in the validation data
+#     - overall_map: Average Mean average precision @ k across users in the validation data
+#     """
+#     user_recommendations = {}
+#     user_history = {}
+#     user_precision = {}
+#     user_recall = {}
+#     user_map = {}
+
+#     # Randomly sample user ids from the validation set
+#     user_ids = list(valid_data["user_id"].unique())
+#     sampled_user_ids = random.sample(user_ids, int(0.001*len(user_ids)))
+
+#     for user_id in sampled_user_ids:
+        
+#         # Output a dictionary containing n_sample predicted items for each user.
+#         user_recommendations[user_id] = recommendation_sys(user_id, cosine_similarity_matrix, train_data, all_user_toread, all_books, n_sample)
+    
+#         # Output a dictionary containing actual rated items for each user.
+#         user_history[user_id] = valid_data.loc[valid_data["user_id"] == id, "book_id"].tolist()
+
+#         # Get lists of predicted & actual items for the user, default to an empty list
+#         predicted = user_recommendations.get(user_id, [])
+#         actual = user_history.get(user_id, [])
+    
+#         # Output a dictionary containing metric scores for each user 
+#         user_precision[id] = calculate_precision_at_k(actual, predicted, top_k)
+#         user_recall[id] = calculate_recall_at_k(actual, predicted, top_k)
+#         user_map[id] = calculate_mean_average_precision_at_k(actual, predicted, top_k)
+        
+#     # Average metric over all users
+#     overall_precision = sum(user_precision.values()) / len(user_precision)
+#     overall_recall = sum(user_recall.values()) / len(user_recall)
+#     overall_map = sum(user_map.values()) / len(user_map)
+
+#     return overall_precision, overall_recall, overall_map
 
 ###############################################################################################
 
